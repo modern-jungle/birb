6497a9b1f52fabf09ceae8b0c35690d44e6d2ff2
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2020-02-08 21:09:53 -0500

    feat: url-friendly slugify

--- api/article/models/article.js
+++ api/article/models/article.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const slugify = require("slugify");
+const slugifyOptions = { remove: /[*+~.()'"!:@]/g, lower: true };
 
 module.exports = {
   beforeSave: async (model, attrs, options) => {
     if (options.method === "insert" && attrs.title) {
-      model.set("slug", slugify(attrs.title));
+      model.set("slug", slugify(attrs.title, slugifyOptions));
     } else if (options.method === "update" && attrs.title) {
-      attrs.slug = slugify(attrs.title);
+      attrs.slug = slugify(attrs.title, slugifyOptions);
     }
   }
 };
